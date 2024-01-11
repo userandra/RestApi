818aa6abe4a720cfc6f4a26f5dc041f5b0bab08b
Andra Andra <andra.ahmed@yahoo.com>
2024-01-12 00:38:07 +0200

    commit initial

---
+++ src/main/java/Test/RestExample.java
@@ -0,0 +1,16 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+
+
+public class RestExample {
+	
+	@Test  //add test ng libraries si apoi import adnotarea Test
+	public void restExampleTest() {
+		
+		
+	}
+
+}
